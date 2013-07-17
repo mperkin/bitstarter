708cc625b6df2cc9ad1fcb6ffe14f8d7e8290106
Mark Perkin <mp1971@gmail.com>
2013-07-17 11:43:39 +0000

    Load welcome from index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var buffer = fs.readFileSync( 'index.html' );
+
+var welcome = buffer.toString();
+
+p.get('/', function(request, response) {
+  response.send( welcome );
 });
 
 var port = process.env.PORT || 5000;
